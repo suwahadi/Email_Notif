086b5e0955a4563e3593f63544d4501cbb1dd560
suwahadi <suwahadi@gmail.com>
2014-01-25 20:11:53 +0700

    Iseng :D

--- woocommerce_emails/admin-new-order_27122013.php
+++ woocommerce_emails/admin-new-order_27122013.php
@@ -4,19 +4,11 @@
 * Corat-coret by Suwahadi 25 Jan 2014
 * 
 */
-
 <?php if ( $target->status == 'jomblo' ) : ?>
-
 <p><?php printf( __( 'Cihuuuy, ajak jadian bro...') ); ?></p>
-
 <?php elseif ( 'pikirpikir' == $target->status ) : ?>
-
 <p><?php printf( __( 'Sabar, tunggu momen yg tepat!') ); ?></p>
-
 <?php elseif ( 'pacaran' == $target->status ) : ?>
-
 <p><?php printf( __( 'Silahkan ber-galau, Jones...') ); ?></p>
-
 <?php endif; ?>
-
 ?>
